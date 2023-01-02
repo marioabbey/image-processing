5a99613cd44d8c5174d3f53d6eb6491df1c9b30a
marioabbey <5434406+marioabbey@users.noreply.github.com>
2023-01-02 05:28:04 +0100

    The Node.js scripts that exist in the package.json file include: `npm run lint` for using eslint for code linting; `npm run build` to use TypeScript to build the `dist` production folder, `npm run prettier` to format files; `npm run dev` for starting the development server with Nodemon; `npm start` to use node to run the `index.js` file found in the `/dist` production build folder; `npm run test` will do a production build, and runs the Jasmine Unit Tests.  The main focus of the tests is to verify API routes return HTTP status of 200, and verify generated test images.

--- src/routes/utilities/resize.ts
+++ src/routes/utilities/resize.ts
@@ -1,12 +1,20 @@
-import sharp from "sharp";
-import path from "path";
+import sharp from 'sharp';
+import path from 'path';
 
-
-async function resize(full: string | sharp.SharpOptions | undefined, width: number, height: number){
-    const resizeImage = await sharp(path.join(__dirname, "../../assets/full", `${full}.jpg`))
+async function resize(
+  full: string | sharp.SharpOptions | undefined,
+  width: number,
+  height: number
+) {
+  await sharp(path.join(__dirname, '../../assets/full', `${full}.jpg`))
     .resize(width, height)
-    .toFile(path.join(__dirname,"../../assets/thumb",`${full}-${width}x${height}.jpg`))
-    
+    .toFile(
+      path.join(
+        __dirname,
+        '../../assets/thumb',
+        `${full}-${width}x${height}.jpg`
+      )
+    );
 }
 
 export default resize;
