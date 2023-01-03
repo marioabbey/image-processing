2cbc4c78476cec71cc2328f9c9c4850b3c5ad040
marioabbey <5434406+marioabbey@users.noreply.github.com>
2023-01-03 12:21:52 +0100

    Update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,30 +6,28 @@ import fs from 'fs';
 //define a router for the default homepage
 const routes = express.Router();
 
+try {
+  fs.mkdirSync(path.resolve('build', 'assets', 'thumb'), {
+    recursive: true,
+  });
+} catch (err) {
+  console.error('could not create directory', err);
+}
+try {
+  fs.mkdirSync(path.resolve('build', 'assets', 'full'), {
+    recursive: true,
+  });
+} catch (err) {
+  console.error('could not create directory', err);
+}
+const TestImage = path.resolve('src', 'assets', 'full', 'palmtunnel.jpg');
+const ImgOutput = path.resolve('build', 'assets', 'full', 'palmtunnel.jpg');
 
-  try {
-    fs.mkdirSync(path.resolve('build', 'assets', 'thumb'), {
-      recursive: true,
-    });
-  } catch (err) {
-    console.error('could not create directory', err);
-  }
-  try {
-    fs.mkdirSync(path.resolve('build', 'assets', 'full'), {
-      recursive: true,
-    });
-  } catch (err) {
-    console.error('could not create directory', err);
-  }
-  const TestImage = path.resolve('src', 'assets', 'full', 'palmtunnel.jpg');
-  const ImgOutput = path.resolve('build', 'assets', 'full', 'palmtunnel.jpg');
-
-  try {
-    fs.copyFileSync(TestImage, ImgOutput);
-  } catch (err) {
-    console.error('System could not copy file', err);
-  }
-
+try {
+  fs.copyFileSync(TestImage, ImgOutput);
+} catch (err) {
+  console.error('System could not copy file', err);
+}
 
 routes.get('/', function (req: express.Request, res: express.Response): void {
   res.send('default page');
